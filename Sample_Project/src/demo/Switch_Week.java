8603d60143bc832006dc08f516da3408c572f904
Dell <Dell@DESKTOP-SH3IHAV>
2022-06-17 15:57:37 -1200

    "Added new class "

---
+++ Sample_Project/src/demo/Switch_Week.java
@@ -0,0 +1,31 @@
+package demo;
+
+public class Switch_Week
+{
+
+	public static void main(String[] args) 
+	{
+		int a=9;
+		switch(a)
+		{
+		case 1:System.out.println("Sunday");
+		break;
+		case 2:System.out.println("Monday");
+		break;
+		case 3:System.out.println("Tuesday");
+		break;
+		case 4:System.out.println("Wendesday");
+		break;
+		case 5:System.out.println("Thursday");
+		break;
+		case 6:System.out.println("Friday");
+		break;
+		case 7:System.out.println("Saturday");
+		break;
+		default:System.out.println("Invalid Input");
+		break;
+		}
+
+	}
+
+}
